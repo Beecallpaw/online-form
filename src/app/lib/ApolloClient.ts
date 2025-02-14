347ea5bd66172d359bd55f96a3fc4d505ffb18d8
Bikalpa <bikalpa.thapa@proshore.eu>
2025-02-14 18:38:10 +0545

    Use next public url

--- src/app/lib/ApolloClient.ts
+++ src/app/lib/ApolloClient.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-const URI = process.env.URI || 'http://localhost:3000'
+const URI = process.env.NEXT_PUBLIC_URI || 'http://localhost:3000'
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: `${URI}/api/graphql`, fetch }),
