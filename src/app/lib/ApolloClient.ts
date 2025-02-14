7208f1b5948dc34669d9ff31c6a442b96abbab57
Bikalpa <bikalpa.thapa@proshore.eu>
2025-02-14 19:03:41 +0545

    Vercel fix

--- src/app/lib/ApolloClient.ts
+++ src/app/lib/ApolloClient.ts
@@ -1,5 +1,5 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
-const URI = process.env.URI || 'http://localhost:3000'
+const URI = process.env.NEXT_PUBLIC_URI || 'https://online-form-sandy.vercel.app'
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: `${URI}/api/graphql`, fetch }),
